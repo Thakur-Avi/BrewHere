329b0da4c142da332fe4a3ee725d71e930e38529
Etika <etika8445@gmail.com>
2024-08-11 14:42:03 +0530

    Update Compiler

--- Coding-platform-Frontend/src/components/Playground/Compiler.jsx
+++ Coding-platform-Frontend/src/components/Playground/Compiler.jsx
@@ -1,15 +1,17 @@
 import CodeEditor from "./CodeEditor"
+import { useState } from "react"
 import "./Compiler.css"
 import EditorInput from "./EditorInput"
 import OutputDisplay from "./OutputDisplay"
 
 const Compiler = () => {
+  const [input, setinput] = useState();
   return (
     <>
     <div className="layout-compiler">
         <div className="edit-compiler">
-            <CodeEditor/>
-            <EditorInput/>
+            <CodeEditor input={input}/>
+            <EditorInput Setinput={setinput}/>
         </div>
         <div className="output-compiler">
             <OutputDisplay/>
